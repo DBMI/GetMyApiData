65eaf5105087f96ba45c058fed40b24ff1d59268
Kevin J. Delaney <kjdelaney@ucsd.edu>
2025-09-22 12:39:27 -0700

    Leave OK button enabled

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GetMyApiData',
-    version='0.1.4',
+    version='0.1.5',
     packages=['getmyapidata'],
     package_dir={'getmyapidata': 'src/getmyapidata'},
     url='https://github.com/DBMI/GetMyApiData',
