92c78e70a0d2f817bca52c201d787420f0c0c091
Kevin J. Delaney <kjdelaney@ucsd.edu>
2025-09-29 15:36:33 -0700

    Skipping gcloud tests (for now)

--- tests/test_gcloud_tools.py
+++ tests/test_gcloud_tools.py
@@ -1,10 +1,12 @@
 """
 Tests methods in class GCloudTools
 """
+import pytest
 
 from src.getmyapidata.gcloud_tools import GCloudTools
 
 
+@pytest.mark.skip(reason="No need to burn up token allotment.")
 def test_instantiation(fake_aou_package, fake_logger) -> None:
     gcloud_tools: GCloudTools = GCloudTools(
         aou_package=fake_aou_package, log=fake_logger
